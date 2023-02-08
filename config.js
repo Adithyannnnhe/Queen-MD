ccd5c29fdd698a0de8f0232eeaf91ef85630a1ff
Adithyannnnhe <124568590+Adithyannnnhe@users.noreply.github.com>
2023-02-08 15:54:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'R1RTanRaU3k=',
+global.ownername = process.env.OWNER_NAME === undefined ? "RAGNAROK" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '9072759552 : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
